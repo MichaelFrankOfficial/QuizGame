5b022061d23bd7d927433f4f659d71192292adfd
Nic B <n1scby@yahoo.com>
2017-12-07 18:29:05 -0500

    added some functionality and two more buttons

--- quiz.js
+++ quiz.js
@@ -6,12 +6,17 @@ var teamOne = document.getElementById("teamOne"),
     btnRandomTeam = document.getElementById("btnRandomTeam"),
     btnAwardPoints = document.getElementById("btnAwardPoints")
 btnNextQuestion = document.getElementById("btnNextQuestion"),
+    btnStartOver = document.getElementById("btnStartOver"),
+    btnSwitchTeam = document.getElementById("btnSwitchTeam"),
     questionTeamName = document.getElementById("questionTeamName"),
     questionNumber = document.getElementById("questionNumber"),
     questionInfo = document.getElementById("questionInfo"),
+    gameSection = document.getElementById("gameSection"),
+    teamEntry = document.getElementById("teamEntry"),
     scoreOne = document.getElementById("scoreOne"),
     scoreTwo = document.getElementById("scoreTwo");
 
+var questions = [];
 var qNum = 0;
 
 var scoreOneNum = 0;
@@ -20,7 +25,7 @@ var scoreTwoNum = 0;
 var nextTeam = 1;
 
 
-var questions = ["What is a function?", "What is a method?", "What are the 3 or 4 pillars of OOP? (5 pts each)",
+var questionList = ["What is a function?", "What is a method?", "What are the 3 or 4 pillars of OOP? (5 pts each)",
     "Name the three types of programming styles we discussed. (5 pts each)", "What is the “gorilla and banana problem”?",
     "Describe Inheritance.", "Describe polymorphism.", "Name three HTML 5 elements used to semantically organize a web page.",
     "Describe one reason how using HTML 5 sections could be beneficial for some of your users.",
@@ -51,7 +56,8 @@ var questions = ["What is a function?", "What is a method?", "What are the 3 or 
                 <pre>var greetUser = function greetUser(<span class="bg-warning">name</span>){
                 return "Hello, " + name;
                 }</pre>`];
-var getRandomNumber = function getRandomNumber(min, max){
+
+var getRandomNumber = function getRandomNumber(min, max) {
     max = Math.floor(max);
     min = Math.floor(min);
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -64,29 +70,62 @@ btnStartGame.addEventListener("click", function () {
         alert("Please Make Sure Both Fields Are Filled Out Then Press Start Game!");
     }
     else {
+        teamEntry.className = "d-none";
+        gameSection.className = "d-block";
+        btnAwardPoints.disabled = true;
+        btnSwitchTeam.disabled = true;
+        setupQuestions();
         scoreOneNum = 0;
         teamOneScore.innerHTML = teamOne.value + " : " + scoreOneNum;
         scoreTwoNum = 0;
         teamTwoScore.innerHTML = teamTwo.value + " : " + scoreTwoNum;
-        questionInfo.innerHTML = "Welcome to the Quiz Game! First start of by selecting a team at random using the button above. <br />Click the next question button located above to go to the first question!";
+        questionInfo.innerHTML = "Welcome to the Quiz Game! A team will be randomly selected for each question. <br />Click the next question button located above to go to the first question!";
     }
 });
+
+btnStartOver.addEventListener("click", function () {
+    teamEntry.className = "d-block";
+    gameSection.className = "d-none";
+    teamEntry.reset();
+    qNum = 0;
+    questionTeamName.innerHTML = "Quiz Game!";
+    questionNumber.innerHTML = "";
+    
+})
+
+btnSwitchTeam.addEventListener("click", function(){
+    if(nextTeam == 1){
+        nextTeam = 2;
+        questionTeamName.innerHTML = teamTwo.value + "'s Question!"
+    } else {
+        nextTeam = 1;
+        questionTeamName.innerHTML = teamOne.value + "'s Question!"
+    }
+})
+
+
 btnNextQuestion.addEventListener("click", function () {
-    if (qNum <= questions.length - 1) {
-        questionInfo.innerHTML = questions[qNum];
+    //   if (qNum <= questions.length - 1) {
+
+    if (questions.length > 0) {
+        //      questionInfo.innerHTML = questions[qNum];
+        questionInfo.innerHTML = getQuestion();
         qNum++;
         questionNumber.innerHTML = "Question # " + qNum;
-
-        nextTeam = getRandomNumber(1,2);
-        if(nextTeam == 1){
+        btnAwardPoints.disabled = false;
+        btnSwitchTeam.disabled = false;
+        nextTeam = getRandomNumber(1, 2);
+        if (nextTeam == 1) {
             questionTeamName.innerHTML = teamOne.value + "'s Question!"
         }
-        else{
+        else {
             questionTeamName.innerHTML = teamTwo.value + "'s Question!"
         }
 
     }
     else {
+        btnAwardPoints.disabled = true;
+        btnSwitchTeam.disabled = true;
         if (scoreOneNum > scoreTwoNum) {
             questionInfo.innerHTML = teamOne.value + " <strong>YOU'RE SO COOL OMG YOU JUST WON A QUIZ GAME WWWOOOOOOWWWWWW ";
             questionNumber.innerHTML = " ";
@@ -97,15 +136,38 @@ btnNextQuestion.addEventListener("click", function () {
             questionNumber.innerHTML = " ";
             questionTeamName.innerHTML = teamTwo.value + "'s THE WINNER";
         }
+        else {
+            questionInfo.innerHTML = "<strong>TIE GAME!  Whoever gives Michael $50 first wins the tiebreaker!</strong> ";
+            questionNumber.innerHTML = " ";
+            questionTeamName.innerHTML = "TIE GAME";
+
+        }
     }
 });
 btnAwardPoints.addEventListener("click", function () {
-    if(nextTeam == 1){
+    if (nextTeam == 1) {
         scoreOneNum += 5;
         teamOneScore.innerHTML = teamOne.value + " : " + scoreOneNum;
     }
-    else if(nextTeam == 2){
+    else if (nextTeam == 2) {
         scoreTwoNum += 5;
         teamTwoScore.innerHTML = teamTwo.value + " : " + scoreTwoNum;
     }
+    btnAwardPoints.disabled = true;
+    btnSwitchTeam.disabled = true;
 });
+
+function setupQuestions() {
+    questionList.forEach(function (item) {
+        questions.push(item);
+    })
+}
+
+function getQuestion() {
+    var qNumber = getRandomNumber(0, questions.length - 1);
+    var question = questions[qNumber];
+    questions.splice(qNumber, 1);
+    return question;
+}
+
+
